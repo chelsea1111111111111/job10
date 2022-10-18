69958132a6d49d9b94965f72b26f51ea61f3ecf1
job10 <dasdwasd@gmail.com>
2022-10-18 13:51:38 +0700

    first commit

---
+++ app/src/main/java/com/app/ceng_job10/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.app.ceng_job10
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
